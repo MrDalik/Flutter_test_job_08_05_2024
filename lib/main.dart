ef744785b655f6919c1f8bb5d0c162bf721c0541
MrDalik <saryginvladislav2004@gmail.com>
2024-05-10 14:59:31 +0300

    Нету изменения цвету у лейблов

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const BottomNavigationBarExampleApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BottomNavigationBarExampleApp extends StatelessWidget {
+  const BottomNavigationBarExampleApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0x000c0c0c)),
+      home: const Menu(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Menu extends StatefulWidget {
+  const Menu({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Menu> createState() => _MenuState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MenuState extends State<Menu> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Column(
+      children: [
+        Text(
+          'Index 0: Home',
+          style: optionStyle,
+        ),
+      ],
+    ),
+    Text(
+      'Пустота',
+      style: optionStyle,
+    ),
+    Text(
+      'Пустота',
+      style: optionStyle,
+    ),
+    Text(
+      'Пустота',
+      style: optionStyle,
+    ),
+    Text(
+      'Пустота',
+      style: optionStyle,
+    ),
+  ];
 
-  void _incrementCounter() {
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        // showSelectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: true,
+        backgroundColor: const Color(0xFF0c0c0c),
+        selectedLabelStyle:
+            const TextStyle(color: Color(0xFF9F9F9F), fontSize: 10),
+        unselectedLabelStyle:
+            const TextStyle(color: Color(0xFFD54545), fontSize: 10),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icon/airplane.svg",
+                colorFilter: ColorFilter.mode(
+                    _selectedIndex == 0 ? const Color(0xFF2261BC):const Color(0xff5E5F61),
+                    BlendMode.srcIn)),
+            label: 'Авиабилеты',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icon/hotels.svg",
+                colorFilter: ColorFilter.mode(
+                    _selectedIndex == 1 ?const Color(0xFF2261BC):const Color(0xff5E5F61),
+                    BlendMode.srcIn)),
+            label: 'Отели',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icon/short.svg",
+                colorFilter: ColorFilter.mode(
+                    _selectedIndex == 2 ? const Color(0xFF2261BC):const Color(0xff5E5F61),
+                    BlendMode.srcIn)),
+            label: 'Короче',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icon/subscriptions.svg",
+                colorFilter: ColorFilter.mode(
+                    _selectedIndex == 3 ? const Color(0xFF2261BC):const Color(0xff5E5F61),
+                    BlendMode.srcIn)),
+            label: 'Подписки',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icon/profile.svg",
+                colorFilter: ColorFilter.mode(
+                    _selectedIndex == 4 ? const Color(0xFF2261BC):const Color(0xff5E5F61) ,
+                    BlendMode.srcIn)),
+            label: "Профиль",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
